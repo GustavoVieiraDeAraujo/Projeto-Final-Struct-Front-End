d46c34ea8c15fd7ce55275f8fc46eae39a29c5b9
Miguel <miguelcarvalhodemedeiros.mcm@gmail.com>
2022-06-26 14:23:27 -0300

    Preparada a pag de contato para a integração com a api

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -1,10 +1,62 @@
+import { useEffect, useState } from "react"
+import { ButtomForms } from "../../components/buttomFoms/styles"
+import { api, photo } from "../../services/api"
+import { Container } from "./styles"
 
 const Contato = () => {
 
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        api.get('services/index').then(response => setServices(response.data.sort((a,b) => {
+            return a.id - b.id
+        })))
+    }, [])
+    const service_colors = ["#0A369F", "#2DA2FF", "#1D7CFA","#0A369F"]
+
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [service_id, setServiceId] = useState(0)
+    const [about, setAbout] = useState('')
+
     return (
-        <>
-            <h3>Contato</h3>
-        </>
+        <Container>
+            <h1>Contato</h1>
+
+            <div className="form">
+                <div className="name">
+                    <h2>Nome</h2>
+                    <input onChange={(event) => setName(event.target.value)} placeholder='Seu Nome' />
+                </div>
+                <div className="email">
+                    <h2>Email</h2>
+                    <input onChange={(event) => setEmail(event.target.value)} type='email' placeholder="Seu_Email@email.com"/>
+                </div>
+                <div className="service">
+                    <h2>Serviço Desejado</h2>
+                    <div className="service_div">
+                        {services.map((item, number) => {
+                            return(
+                            <label className="service_label" style={service_id == item.id ? {backgroundColor: service_colors[number], border: "4px solid #FD8D4B"} : {backgroundColor: service_colors[number]}}>
+                                <span className="service_span">
+                                    <p className="service_p">{item.name}</p>
+                                    <img src={photo.defaults.baseURL + item.images_url[0]} className="service_image" />
+                                </span>
+                                <input onChange={(event) => setServiceId(event.target.value)} type="radio" id={item.name} name="service" value={item.id} className="service_input"/>
+                            </label>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className="sobre">
+                    <h2>Sobre</h2>
+                    <textarea onChange={(event) => setAbout(event.target.value)} className="sobre_input" placeholder="Como você quer que seja o seu produto"/>
+                </div>
+
+            </div>
+            <ButtomForms onClick={() => console.log(name, email, service_id, about)} >Criar</ButtomForms>
+        </Container>
     )
 }
 
---
+++ src/pages/contato/styles.js
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 5.9rem;
+
+    h1 {
+        color: #FD8D4B;
+        font-size: 2.5rem;
+        font-weight: 700;
+    }
+
+    .form {
+        align-items: left;
+        width: 75%;
+    }
+
+
+    h2 {
+        margin-bottom: 0;
+        font-size: 32px;
+        font-weight: 400;
+    }
+
+    input, textarea {
+        position: relative;
+        left: 2%;
+        margin-bottom: 3rem;
+        margin-top: 0;
+        width: 95%;
+        height: 3,625rem;
+        font-weight: 300;
+        font-size: 2rem;
+        padding: 1rem;
+        background-color: #0A369F69;
+        border: none;
+        color: #F8F8FF;
+    }
+
+    .sobre_input {
+        height: 25rem;
+        resize: none;
+    }
+    
+    .service_div{
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .service_label {
+        box-sizing: border-box;
+        display: flex;
+        border-radius: 33px;
+        width: 200px;
+        height: 231px;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        justify-content: center;
+        padding-top: 0;
+
+        transition: border 0.1s ease-in-out;
+    }
+
+    .service_image {
+        min-width: 95px;
+        max-width: 164px;
+        max-height: 125px;
+        height: auto;
+        align-self: center;
+        position: relative;
+        top: -1.5rem;
+    }
+
+    .service_input {
+        width: 0;
+        height: 0;
+        margin: 0;
+        opacity: 0;
+        position: absolute;
+    }
+
+    .service_span{
+        width:100%;
+        display:grid;
+        grid-auto-flow: row;
+        justify-content: center;
+        justify-items: center;
+        ${'' /* flex-direction: column;  */}
+        ${'' /* align-content: center; */}
+        margin: 0;
+        padding: 0;
+
+    }
+
+    .service_p {
+        margin: 1rem;
+        padding: 0;
+        font-weight: 400;
+        font-size: 20px;
+
+    }
+
+`
